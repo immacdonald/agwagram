262d7e6ec646c3927db29ad2d2b836a8d7d0a08a
Ian Michael MacDonald <phantomdonut@yahoo.com>
2023-04-05 14:21:57 -0400

    Added basic form validator and error handling

--- blocdemo/code/bloc_handler.py
+++ blocdemo/code/bloc_handler.py
@@ -6,38 +6,61 @@ from argparse import Namespace
 from bloc.generator import gen_bloc_for_users
 from bloc.subcommands import run_subcommands
 
-
-def analyze_user(screen_name):
+def verify_user_exists(screen_name):
     oauth2 = osometweet.OAuth2(bearer_token=settings.BEARER_TOKEN, manage_rate_limits=False)
     ot = osometweet.OsomeTweet(oauth2)
 
     # Returns dict list with 'id' 'name' and 'username' fields
-    user_data = ot.user_lookup_usernames([screen_name])['data']
-    user_ids = [ user['id'] for user in user_data ]
+    user_data = ot.user_lookup_usernames([screen_name])
 
-    gen_bloc_params, gen_bloc_args = get_bloc_params(user_ids, settings.BEARER_TOKEN, bloc_alphabets=['action', 'content_syntactic', 'content_semantic_entity'])
-    bloc_payload = gen_bloc_for_users(**gen_bloc_params)
+    if(user_data.get('errors')):
+        error_details = {
+            'error_title': user_data['errors'][0]['title'],
+            'error_detail': user_data['errors'][0]['detail']
+        }
+        return False, error_details
 
-    all_bloc_output = bloc_payload.get('all_users_bloc', [])
-    #total_tweets = sum([ user_bloc['more_details']['total_tweets'] for user_bloc in bloc_payload ])
+    print('User data', user_data)
 
-    #pairwise_sim_report = run_subcommands(gen_bloc_args, 'sim', all_bloc_output)
-    top_k_bloc_words = run_subcommands(gen_bloc_args, 'top_ngrams', all_bloc_output)
-    
-    result = {
-        # User Data
-        'account_name': user_data[0]['name'],
-        # BLOC Statistics
-        'tweet_count': all_bloc_output[0]['more_details']['total_tweets'],
-        'first_tweet_date': all_bloc_output[0]['more_details']['first_tweet_created_at_local_time'],
-        'last_tweet_date': all_bloc_output[0]['more_details']['last_tweet_created_at_local_time'],
-        'elapsed_time': all_bloc_output[0]['elapsed_time']['gen_tweets_total_seconds'] + all_bloc_output[0]['elapsed_time']['gen_bloc_total_seconds'],
-        # Analysis
-        'bloc_action': all_bloc_output[0]['bloc']['action'],
-        'bloc_content_syntactic': all_bloc_output[0]['bloc']['content_syntactic'],
-        'bloc_content_semantic': all_bloc_output[0]['bloc']['content_semantic_entity'],
-        'top_bloc_words': top_k_bloc_words['per_doc'][0]
-    }
+    return True, user_data['data']
+
+
+def analyze_user(screen_name):
+    user_exists, user_data = verify_user_exists(screen_name)
+
+    if not user_exists:
+        result = {
+            'user_exists': False,
+            'error_title': user_data['error_title'],
+            'error_detail': user_data['error_detail']
+        }
+
+    else:
+        user_ids = [ user['id'] for user in user_data ]
+        gen_bloc_params, gen_bloc_args = get_bloc_params(user_ids, settings.BEARER_TOKEN, bloc_alphabets=['action', 'content_syntactic', 'content_semantic_entity'])
+        bloc_payload = gen_bloc_for_users(**gen_bloc_params)
+
+        all_bloc_output = bloc_payload.get('all_users_bloc', [])
+        #total_tweets = sum([ user_bloc['more_details']['total_tweets'] for user_bloc in bloc_payload ])
+
+        #pairwise_sim_report = run_subcommands(gen_bloc_args, 'sim', all_bloc_output)
+        top_k_bloc_words = run_subcommands(gen_bloc_args, 'top_ngrams', all_bloc_output)
+        
+        result = {
+            'user_exists': True,
+            # User Data
+            'account_name': user_data[0]['name'],
+            # BLOC Statistics
+            'tweet_count': all_bloc_output[0]['more_details']['total_tweets'],
+            'first_tweet_date': all_bloc_output[0]['more_details']['first_tweet_created_at_local_time'],
+            'last_tweet_date': all_bloc_output[0]['more_details']['last_tweet_created_at_local_time'],
+            'elapsed_time': all_bloc_output[0]['elapsed_time']['gen_tweets_total_seconds'] + all_bloc_output[0]['elapsed_time']['gen_bloc_total_seconds'],
+            # Analysis
+            'bloc_action': all_bloc_output[0]['bloc']['action'],
+            'bloc_content_syntactic': all_bloc_output[0]['bloc']['content_syntactic'],
+            'bloc_content_semantic': all_bloc_output[0]['bloc']['content_semantic_entity'],
+            'top_bloc_words': top_k_bloc_words['per_doc'][0]
+        }
 
     return result
 
--- blocdemo/views.py
+++ blocdemo/views.py
@@ -24,41 +24,44 @@ def methodology(request):
     return render(request, 'methodology.html')
 
 def analysis_results(request):
-    # Default search for OSoMe_IU
-    username = "Invalid Search"
-   
-    if request.method == "POST":
-        #Get the posted form
-        usernameInputForm = UsernameSearchForm(request.POST)
-      
-        if usernameInputForm.is_valid():
-            username = usernameInputForm.cleaned_data['username']
-    else:
-        usernameInputForm = UsernameSearchForm()
-        return redirect('main')
+    form = UsernameSearchForm(request.POST or None)
+    print('Valid form?', form.is_valid())
+    if not form.is_valid():
+        return render(request, 'analyze.html', {'form': form})
 
+    username = form.cleaned_data['username']
     results = bloc_handler.analyze_user(username)
 
-    # Output formatting
-    for word in results['top_bloc_words']:
-        word['term_rate'] = "{:.3f}".format(word["term_rate"], 3)
+    if results['user_exists']:
+        # Output formatting
+        for word in results['top_bloc_words']:
+            word['term_rate'] = "{:.3f}".format(word["term_rate"], 3)
 
-    initial_date_format = '%Y-%m-%d %H:%M:%S'
-    output_date_format = '%m/%d/%Y'
+        initial_date_format = '%Y-%m-%d %H:%M:%S'
+        output_date_format = '%m/%d/%Y'
 
-    context = {
-        # User Data
-        "username" : username, 
-        "account_name": results['account_name'],
-        # BLOC Statistics
-        'tweet_count': results['tweet_count'],
-        'first_tweet_date': datetime.strptime(results['first_tweet_date'], initial_date_format).strftime(output_date_format),
-        'last_tweet_date': datetime.strptime(results['last_tweet_date'], initial_date_format).strftime(output_date_format),
-        'elapsed_time': round(results['elapsed_time'], 3),
-        # Analysis
-        "bloc_action": results['bloc_action'].replace(' ', '&nbsp;'),
-        "bloc_content_syntactic": results['bloc_content_syntactic'].replace(' ', '&nbsp;'),
-        "bloc_content_semantic": results['bloc_content_semantic'].replace(' ', '&nbsp;'),
-        "top_bloc_words": results['top_bloc_words'][:10]
-    }
-    return render(request, 'analysis_results.html', context)
+        context = {
+            # User Data
+            "username" : username, 
+            "account_name": results['account_name'],
+            # BLOC Statistics
+            'tweet_count': results['tweet_count'],
+            'first_tweet_date': datetime.strptime(results['first_tweet_date'], initial_date_format).strftime(output_date_format),
+            'last_tweet_date': datetime.strptime(results['last_tweet_date'], initial_date_format).strftime(output_date_format),
+            'elapsed_time': round(results['elapsed_time'], 3),
+            # Analysis
+            "bloc_action": results['bloc_action'].replace(' ', '&nbsp;'),
+            "bloc_content_syntactic": results['bloc_content_syntactic'].replace(' ', '&nbsp;'),
+            "bloc_content_semantic": results['bloc_content_semantic'].replace(' ', '&nbsp;'),
+            "top_bloc_words": results['top_bloc_words'][:10]
+        }
+        return render(request, 'analysis_results.html', context)
+    
+    else:
+        context = {
+            # User Data
+            "username" : username, 
+            'error_title': results['error_title'],
+            'error_detail': results['error_detail']
+        }
+        return render(request, 'analysis_failed.html', context)
