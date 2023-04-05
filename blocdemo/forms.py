799833177002235b7eff6f77fc608d93c0679a86
Ian Michael MacDonald <phantomdonut@yahoo.com>
2023-04-05 15:14:31 -0400

    Added a dark mode toggle to the website

--- blocdemo/forms.py
+++ blocdemo/forms.py
@@ -6,7 +6,7 @@ class UsernameSearchForm(forms.Form):
                               label = 'Search by Username:', 
                               validators=[validators.RegexValidator(
                                     regex=r'^[A-Za-z0-9_]{1,15}$',
-                                    message='Field must contain only letters, numbers, or underscores, and be between 1 and 15 characters long'
+                                    message='Username must contain only letters, numbers, or underscores, and be between 1 and 15 characters long'
                                  )],
                               required = True, 
                               widget=forms.TextInput(attrs={'placeholder': 'OSoMe_IU'}))
