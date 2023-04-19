d0249f08a4c1784d0f91ac40f504e3c278096e2b
Ian Michael MacDonald <phantomdonut@yahoo.com>
2023-04-19 13:41:06 -0400

    Tooltip display for linked data

--- blocdemo/templatetags/custom_filters.py
+++ blocdemo/templatetags/custom_filters.py
@@ -5,4 +5,12 @@ register = template.Library()
 
 @register.filter
 def get_description(symbol):
-    return bloc_symbols.get_all_symbols().get(symbol, '')
+    return ', '.join(bloc_symbols.get_multi_symbol_meanings(symbol, ''))
+
+@register.filter
+def get_type(symbol):
+    return bloc_symbols.get_symbol_type(symbol)
+
+@register.filter
+def index(indexable, i):
+    return indexable[i]
