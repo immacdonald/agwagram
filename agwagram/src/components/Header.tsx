7cacb1752c7ab07c386c0ac86c242c04aa40a43e
Ian MacDonald <ianmacdonaldm@gmail.com>
2023-10-08 22:48:34 -0400

    Updated header and SCSS colors

--- agwagram/src/components/Header.tsx
+++ agwagram/src/components/Header.tsx
@@ -1,15 +1,19 @@
 import DynamicLink from './DynamicLink';
 import style from './Header.module.scss';
+import Logo from '../static/images/agwagram.png';
 
 function Header() {
     return (
         <header className={style.header}>
             <div className={style.content}>
-                <div className={style.logo}>Hollywood Director-Crew Networks</div>
+                <div className={style.logo}>
+                    <img src={Logo} />
+                </div>
                 <div className={style.navigation}>
                     <nav className={style.navLinks}>
                         <DynamicLink link="/" label="Home" />
-                        <DynamicLink link="/about" label="About" />
+                        <DynamicLink link="/analyze" label="Analyze" />
+                        <DynamicLink link="/methodology" label="Methodology" />
                     </nav>
                 </div>
             </div>
