3af67d7647d082f00a1a108f317d203eb9371520
Ian MacDonald <ianmacdonaldm@gmail.com>
2023-12-08 01:15:51 -0500

    Switched database to MySQL

--- bloc/blocservices/settings.py
+++ bloc/blocservices/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = "blocservices.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": BASE_DIR / "db.mysql",
     }
 }
 
