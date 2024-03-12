f1baf23c92ea17c3bcf32c4d46bf7b8acf046416
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-03-11 22:15:46 -0400

    Bump version: 1.7.0 → 1.7.1

--- agwagram/src/Global.tsx
+++ agwagram/src/Global.tsx
@@ -21,4 +21,4 @@ export const graphColor = (index: number): string => {
 	return GRAPH_COLORS[index % GRAPH_COLORS.length];
 };
 
-export const VERSION: string = '1.7.0';
+export const VERSION: string = '1.7.1';
