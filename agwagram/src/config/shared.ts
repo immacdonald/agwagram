9aa5c2f0a06aeef5389e364fe2f7e1715f3ac389
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-06-13 21:35:09 -0400

    Bump version: 1.12.0 → 1.13.0

--- agwagram/src/config/shared.ts
+++ agwagram/src/config/shared.ts
@@ -1,7 +1,7 @@
 const API_BASE: string = window.location.origin == 'http://localhost:8000' ? 'http://localhost:8080' : window.location.origin;
 
 export default {
-	version: '1.12.0',
+	version: '1.13.0',
 	apiURL: `${API_BASE}/api/v1`,
 	exampleFiles: [
 		{ file: 'sample_storygraphbot.jsonl', title: '@StoryGraphBot', type: 'JSONL' },
