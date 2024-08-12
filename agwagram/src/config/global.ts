de595211703fdd1349e7b84138241995ab768387
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-08-11 22:31:04 -0400

    Bump version: 1.20.1 → 1.20.2

--- agwagram/src/config/global.ts
+++ agwagram/src/config/global.ts
@@ -1,7 +1,7 @@
 const API_BASE: string = window.location.origin == 'http://localhost:8000' ? 'http://localhost:8080' : window.location.origin;
 
 const global = {
-    version: '1.20.1',
+    version: '1.20.2',
     apiURL: `${API_BASE}/api/v1`,
     exampleFiles: [
         { file: 'bot_storygraphbot.jsonl.gz', title: 'Bot: @StoryGraphBot', type: 'JSONL' },
