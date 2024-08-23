ddcdd06fc3a19c64aa6efcca6843937d3ed2558a
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-08-23 14:08:56 -0400

    Updated phantom-library dependency and modified production basepath

--- agwagram/src/components/page/Footer/Footer.tsx
+++ agwagram/src/components/page/Footer/Footer.tsx
@@ -1,22 +1,20 @@
 import { Link } from 'react-router-dom';
 import { FC } from 'react';
-import style from './Footer.module.scss';
+import { SimpleFooter, Text } from 'phantom-library';
 
 const Footer: FC = () => {
     return (
-        <footer className={style.footer}>
-            <div>
-                <span>
-                    &copy; {new Date().getFullYear()}{' '}
-                    <Link to="https://www.wm.edu" target="_blank">
-                        Willam & Mary
-                    </Link>{' '}
-                    <Link to="https://github.com/wm-newslab" target="_blank">
-                        NEWS Lab
-                    </Link>
-                </span>
-            </div>
-        </footer>
+        <SimpleFooter topBorder>
+            <Text as="span">
+                &copy; {new Date().getFullYear()}{' '}
+                <Link to="https://www.wm.edu" target="_blank">
+                    Willam & Mary
+                </Link>{' '}
+                <Link to="https://github.com/wm-newslab" target="_blank">
+                    NEWS Lab
+                </Link>
+            </Text>
+        </SimpleFooter>
     );
 };
 
